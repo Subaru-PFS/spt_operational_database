4cf218ad5e30c8590a2ed419be192f76a0880a66
Kiyoto Yabe <kiyoto.yabe@ipmu.jp>
2019-07-12 16:25:49 +0900

    add fiberId in CobraConfig

--- models.py
+++ models.py
@@ -627,6 +627,7 @@ class CobraConfig(Base):
 
     cobraConfigId = Column(BigInteger, primary_key=True, autoincrement=True)
     pfsConfigFiberId = Column(BigInteger, ForeignKey('pfsConfigFiber.pfsConfigFiberId'))
+    fiberId = Column(Integer)
     iteration = Column(Integer)
     motorNumStepTheta = Column(Integer)
     motorNumStepPhi = Column(Integer)
@@ -641,11 +642,12 @@ class CobraConfig(Base):
     mcsCenter_y = Column(Float(precision=24))
     exectime = Column(DateTime)
 
-    def __init__(self, cobraConfigId, pfsConfigFiberId, iteration, motorNumStepTheta, motorNumStepPhi, mcsId,
-                 pfiNominal_x, pfiNominal_y, pfiCenter_x, pfiCenter_y, pfiDiff_x, pfiDiff_y,
+    def __init__(self, cobraConfigId, pfsConfigFiberId, fiberId, iteration, motorNumStepTheta, motorNumStepPhi,
+                 mcsId, pfiNominal_x, pfiNominal_y, pfiCenter_x, pfiCenter_y, pfiDiff_x, pfiDiff_y,
                  mcsCenter_x, mcsCenter_y, exectime):
         self.cobraConfigId = cobraConfigId
         self.pfsConfigFiberId = pfsConfigFiberId
+        self.fiberId = fiberId
         self.iteration = iteration
         self.motorNumStepTheta = motorNumStepTheta
         self.motorNumStepPhi = motorNumStepPhi
