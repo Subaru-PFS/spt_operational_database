9f9c3d5c2b7a067ceac08f6edbb477eae18e4a72
Craig Loomis <cloomis@astro.princeton.edu>
2021-07-22 20:57:29 -1000

    Add bulkSelect and bulkInsert
    New interfaces for efficient (COPY_{FROM,TO}) bulk
    operations. Requires sqlalchemy 1.4.

--- python/opdb/opdb.py
+++ python/opdb/opdb.py
@@ -328,6 +328,115 @@ class OpDB(object):
         data.seek(0)
         return data
 
+    # These are the pg_type oids for all the known and used PFS data
+    # types. There are many many more. If we really cared there is
+    # probably a complicated way to use the rest of the pg_type table
+    # to construct numpy dtypes.
+    _pgTypes = {16:np.bool,
+                20:np.int64,
+                23:np.int32,
+                700:np.float32,
+                701:np.float64,
+                1043:str,     # varchar, we want variable length string
+                1114:np.dtype('datetime64[us]'),  # nb: numpy does not do timezones
+                }
+
+    def _getColTypes(self, tablename):
+        """Retrieve mapping of numpy dtypes for all the columns in a table"""
+
+        pgdesc = []
+        dtypes = {}
+        with self.engine.connect() as conn:
+            with conn.connection.cursor() as cursor:
+                cursor.execute(f'select * from {tablename} where False')
+                pgdesc = cursor.description
+
+        for col in pgdesc:
+            code = col.type_code
+            dtype = self._pgTypes[code]
+            dtypes[col.name] = dtype
+
+        return dtypes
+
+    def bulkSelect(self, tablename, selectSql=None):
+        '''Efficiently retrieve selected records from a table.
+
+        Returns a pandas dataframe of all the columns, trying to get
+        the numpy types appropriate for the SQL types.
+
+        The tablename and the selectSql statement can be
+        incompatible. We do not check; the only effect would be that
+        the numpy dtypes will be whatever pandas intuits.
+
+        The selectSql statement can be *any* single SELECT statement
+        which our versinof postgresql accepts in COPY TO statments. If
+        no statement is passed the entire table is returned. Caveat
+        executor, or something.
+
+        Parameters
+        ----------
+        tablename : `str`
+            The tablename. If not None we fetch the table's column types.
+        selectSql : `str`
+            The optional sql select statement.
+
+        Returns
+        -------
+        data      : `pandas.DataFrame`
+           Always contains all columns.
+        '''
+
+        if tablename is not None:
+            dtypes = self._getColTypes(tablename)
+        else:
+            dtypes =  None      # Let pandas auto-detect dtypes
+
+        if selectSql is None:
+            sql = f'COPY {tablename}'
+        else:
+            sql = f'COPY ({selectSql})'
+
+        sqlCmd = f"{sql} TO STDOUT WITH (FORMAT csv, HEADER)"
+        self.logger.info(f'fetching sql: {sqlCmd}')
+
+        buf = io.StringIO()
+        with self.engine.connect() as conn:
+            with conn.connection.cursor() as cursor:
+                cursor.copy_expert(sqlCmd, buf)
+        if buf.tell() == 0:
+            self.logger.warn(f'no data read for: {sqlCmd}')
+            return None
+
+        buf.seek(0, 0)
+        df = pd.read_csv(buf, header=0, dtype=dtypes)
+        return df
+
+    def bulkInsert(self, tablename, data):
+        '''Efficiently insert DataFrame rows into a table.
+
+        UNTESTED as of 2021-07-22
+
+        The data needs to match the structure of the named table well
+        enough. We do not check at all.
+
+        Parameters
+        ----------
+        tablename : `string`
+            The tablename to insert into.
+        data : `pandas.DataFrame`
+            The data to insert.
+        '''
+
+        buf = io.StringIO()
+        data.to_csv(buf, header=False)
+        buf.seek(0)
+
+        sqlCmd = "COPY {tablename} FROM STDIN WITH (FORMAT csv)"
+
+        with self.engine.connect() as conn:
+            with conn.connection.cursor() as cursor:
+                cursor.copy_expert(sqlCmd, buf)
+
     def fetch_sps_exposures(self, pfs_visit_id):
         '''
             Description
